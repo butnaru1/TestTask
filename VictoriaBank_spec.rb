b2274c09a24f70ca0d05b6b29b8dbdd04bc64a46
butnaru1 <40775210+butnaru1@users.noreply.github.com>
2019-10-04 21:21:58 +0300

    Add optimisations at html files.

--- VictoriaBank_spec.rb
+++ VictoriaBank_spec.rb
@@ -1,15 +1,15 @@
 require 'rspec'
-require_relative 'victoria_bank'
+require_relative 'VictoriaBank'
 
 describe 'VictoriaBank' do
   it 'parse_accounts return value an array' do
     victoriabank = VictoriaBank.new
-    expect(victoriabank.parse_accounts("#{File.open('Cards and accounts.html').read}")).to be_an_instance_of(Array)
+    expect(victoriabank.parse_accounts("#{File.open('accounts.html').read}")).to be_an_instance_of(Array)
   end
 
   it 'parse_accounts should be equal to account data' do
     victoriabank = VictoriaBank.new
-    accounts = victoriabank.parse_accounts("#{File.open('Cards and accounts.html').read}")
+    accounts = victoriabank.parse_accounts("#{File.open('accounts.html').read}")
     result = accounts[0].as_json
     expect(result).to eq(
                           :name => 'MD22VI225931303201646102',
@@ -23,13 +23,13 @@ describe 'VictoriaBank' do
   it 'parse_transactions return value an array' do
     victoriabank = VictoriaBank.new
     account = 'MD22VI225931303201646102'
-    expect(victoriabank.parse_transactions(account, "#{File.open('Operations history.html').read}")).to be_an_instance_of(Array)
+    expect(victoriabank.parse_transactions(account, "#{File.open('transactions.html').read}")).to be_an_instance_of(Array)
   end
 
   it 'parse_transactions should be show transaction data' do
     victoriabank = VictoriaBank.new
     account = 'MD22VI225931303201646102'
-    transactions = victoriabank.parse_transactions(account, "#{File.open('Operations history.html').read}")
+    transactions = victoriabank.parse_transactions(account, "#{File.open('transactions.html').read}")
     result = transactions[0].as_json
     expect(result).to eq(
                           :date => '2019-10-1',

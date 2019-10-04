f8099f09b4c5e3a6ca87167fdccc05a685c8c574
butnaru1 <40775210+butnaru1@users.noreply.github.com>
2019-10-04 15:43:17 +0300

    Add show example for accounts and transactions.

--- VictoriaBank_spec.rb
+++ VictoriaBank_spec.rb
@@ -7,9 +7,20 @@ describe 'VictoriaBank' do
     expect(victoriabank.parse_accounts("#{File.open('Cards and accounts.html').read}")).to be_an_instance_of(Array)
   end
 
+  it 'parse_accounts should be show account data' do
+    victoriabank = VictoriaBank.new
+    puts JSON.pretty_generate victoriabank.parse_accounts("#{File.open('Cards and accounts.html').read}")
+  end
+
   it 'parse_transactions return value an array' do
     victoriabank = VictoriaBank.new
     account = 'MD22VI225931303201646102'
     expect(victoriabank.parse_transactions(account, "#{File.open('Operations history.html').read}")).to be_an_instance_of(Array)
+  end
+
+  it 'parse_transactions should be show transaction data' do
+    victoriabank = VictoriaBank.new
+    account = 'MD22VI225931303201646102'
+    puts JSON.pretty_generate victoriabank.parse_transactions(account, "#{File.open('Operations history.html').read}")
   end
 end
